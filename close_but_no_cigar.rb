b90b228a10ad997cdb42d6ae3fb43a55bf78cb21
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-11 13:11:33 -0400

    close_but_no_cigar

--- close_but_no_cigar.rb
+++ close_but_no_cigar.rb
@@ -3,22 +3,38 @@
 # array[index] must be used within the function that the array was created and defined in.
 # |first,second| after the .each_with_index, first is always value, and second is always index position.
 
-def array()
-	array = [*1..100]
-		array.each_with_index do |value,index|
-		if value % 15 == 0
-			array[index] = "mined minds"
-			puts array[index]
-		elsif value % 3 == 0
-			array[index] = "mined"
-			puts array[index]
-		elsif value % 5 == 0
-			array[index] = "minds"
-			puts array[index]
-		else
-			puts "#{value}"
-		end
-	end	
+def your_number()
+	puts "\n"
+	puts "Your Number: "
+	your_number = gets.to_s
+	puts "\n"
 end
+
+def winning_numbers()
+	winning_numbers = ["1028","0315","0602","0826"]
+	if winning_numbers.any? { |winning_numbers| winning_numbers.any == your_number }
+		puts "your_number"
+	elsif puts "no match"
+	end
+	puts "\n"
+end
+
+# def array()
+# 	array = [*1..100]
+# 		array.each_with_index do |value,index|
+# 		if value % 15 == 0
+# 			array[index] = "mined minds"
+# 			puts array[index]
+# 		elsif value % 3 == 0
+# 			array[index] = "mined"
+# 			puts array[index]
+# 		elsif value % 5 == 0
+# 			array[index] = "minds"
+# 			puts array[index]
+# 		else
+# 			puts "#{value}"
+# 		end
+# 	end	
+# end
 
 array
