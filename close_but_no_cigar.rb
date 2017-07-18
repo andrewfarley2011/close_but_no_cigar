103e52aa5fd1e8cf94bfcd274cbc00aaca91e0bb
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-18 13:44:42 -0400

    close_but_no_cigar b

--- close_but_no_cigar.rb
+++ close_but_no_cigar.rb
@@ -1,40 +1,74 @@
 
-# |value,index| these can only be called within the method that the .each_with_index is being used with.
-# array[index] must be used within the function that the array was created and defined in.
-# |first,second| after the .each_with_index, first is always value, and second is always index position.
+def lottery(my_number, winners) 
+	
+	winning = [] 
+    
+	if winners.include?(my_number)
+		# winners.include? checks the array winners to see if any of
+		# the elements in that array match the variable (my_number) 
+    
+		winning << my_number
+		# << moves my_number into the array winning
+    
+		puts "You Win!"
 
-def your_number()
-	puts "\n"
-	puts "Your Number: "
-	your_number = gets.to_s
-	puts "\n"
-end
+	else 
+    	
+    	puts "No match!" 
+  
+	end  
+ 
+	p winning
 
-def winning_numbers()
-	winning_numbers = ["1028","0315","0602","0826"]
-	if winning_numbers.any? { |winning_numbers| winning_numbers.any == your_number }
-		puts "your_number"
-	elsif puts "no match"
-	end
-	puts "\n"
-end
+end 
+ 
+ 
+ 
+def close_but_no_cigar(my_number, winners) 
+     
+	close = [] 
+   
+    winners.each do |number| 
+	# executes code for each element in the array winners 
+	# assigns the variable name |number| to each element in that array
+     
+    	count = 0 
+   		# initial counter 
+    
+    	index = 0 
+    	# counter for index position 
+      
+    	n = number.length 
+    	# returns the number of elements in each string in the array winners
+ 
+    		n.times do 
+    		# executes code for each number in the array winners 
 
-# def array()
-# 	array = [*1..100]
-# 		array.each_with_index do |value,index|
-# 		if value % 15 == 0
-# 			array[index] = "mined minds"
-# 			puts array[index]
-# 		elsif value % 3 == 0
-# 			array[index] = "mined"
-# 			puts array[index]
-# 		elsif value % 5 == 0
-# 			array[index] = "minds"
-# 			puts array[index]
-# 		else
-# 			puts "#{value}"
-# 		end
-# 	end	
-# end
+        		if my_number[index] == number[index] 
+        		# [index] looks at the index position of my_number and the
+        		# index position of each element of the array winners
+    
+            		count = count + 1 
+        
+        		end 
+    
+        	index = index + 1 
+        	# moves to the next index position 
+    
+    		end 
+    
+    	if count == number.length - 1 
+    	# counter for close 
+   
+    		close << number 
+    		# << moves number into the empty array close
+ 
+		end 
+ 		
+ 	end 
+   
+	puts "Close, but no cigar." 
+	
+	p close 
 
-array
+end 
--- close_but_no_cigar_test.rb
+++ close_but_no_cigar_test.rb
@@ -1,51 +1,37 @@
 
-require "minitest/autorun"
+require "minitest/autorun" 
+require_relative "close_but_no_cigar.rb" 
+class Close_But_No_Cigar < Minitest::Test  
 
-require_relative "mm_kata_array.rb"
+	def test_empty 
+		# there is no winner in this instance
+		my_number = "1028" 
+ 		winners = [] 
+ 		assert_equal([], lottery(my_number, winners)) 
+	end 
+ 							 
+	def test_win				 
+ 		my_number = "1028" 
+ 		winners = ["1028", "3978", "4344", "6733"] 
+ 		assert_equal(["1028"], lottery(my_number, winners)) 
+ 	end	  
+  						  
+ 	def test_off_by_one        
+ 	 	my_number = "1028" 
+ 	 	winners = ["1020", "3978", "4344", "6733"] 
+ 		assert_equal(["1020"], close_but_no_cigar(my_number, winners)) 
+ 	end	 
+    
+    def test_for_array_containing_winner 
+    	my_number = "12345" 
+    	winners = ["12340", "10111", "21314", "15161"] 
+ 		assert_equal(["12340"], close_but_no_cigar(my_number,winners)) 
+	end 
 
-class Mined_Minds_Kata < Minitest::Test
+    def test_for_longer_numbers 
+  		my_number = "1234567" 
+     	winners = ["1234560", "8910111", "6171819", "2021222"] 
+     	assert_equal(["1234560"], close_but_no_cigar(my_number, winners)) 
+    end 
 
-	def test_1_equals_1
-		assert_equal(1,1)
-	end
-
-	def test_counter
-		assert_equal(100,array.count)
-		assert_equal(Array,array.class)
-	end
-
-# The array[x] is indicative of the index position, which is one less than the value because the index is 0 based.
-
-	def test_1_returns_1
-		assert_equal(1,array[0])
-	end
-
-	def test_2_returns_2
-		assert_equal(2,array[1]) 
-	end
-
-	def test_3_returns_mined
-		assert_equal("mined",array[2])
-	end
-
-	def test_6_returns_mined
-		assert_equal("mined",array[5])
-	end
-
-	def test_5_returns_minds
-		assert_equal("minds",array[4])
-	end
-
-	def test_10_returns_minds
-		assert_equal("minds",array[9])
-	end
-
-	def test_15_returns_mined_minds
-		assert_equal("mined minds",array[14])
-	end
-
-	def test_30_returns_mined_minds
-		assert_equal("mined minds",array[29])
-	end
-
-end
+end 
